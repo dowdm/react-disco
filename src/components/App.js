a1fd547aefb5871839b1d6f215d17b73591cad9b
Matt Dowdney and Matt Miller <matt@>
2018-07-17 16:42:07 -0700

    Add selected record component that shows details.

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react'
 import './App.css'
 import ArtistSearch from './ArtistSearch'
 import RecordList from './RecordList'
+import SelectedRecord from './SelectedRecord'
 import { connect } from 'react-redux';
 
 class App extends React.Component {
 
   render() {
-    let keys = Object.keys(this.props.masterRecordList);
+    let keys = Object.keys(this.props.selectedRecordDetails);
     let displayContent
     console.log(this.state);
     if(keys.length > 0){
+      displayContent =
+      <SelectedRecord/>
+    }
+    else if(this.props.masterRecordList.length > 0){
       displayContent =
         <RecordList/>
     } else {
@@ -27,7 +32,8 @@ class App extends React.Component {
 const mapStateToProps = state => {
   return {
     masterRecordList: state.masterRecordList,
-    queryTerm: state.queryTerm
+    queryTerm: state.queryTerm,
+    selectedRecordDetails: state.selectedRecordDetails
   };
 };
 
---
+++ src/components/SelectedRecord.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { fetchAlbumDetails, setDetails } from './../actions';
+
+function SelectedRecord(props){
+  const {dispatch} = props;
+
+  return (
+    <div >
+    <p onClick={() => dispatch(setDetails({}))}>Go Back</p>
+    <p>{props.selectedRecordDetails.title}</p>
+    <img src={props.selectedRecordDetails.images[0].uri} />
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+  return state;
+};
+
+
+
+export default connect(mapStateToProps)(SelectedRecord);
